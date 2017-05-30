1ceb3c9f8e8e8856dee77206d02212313641311a
jeanphilippeds <jphilippe.ds@gmail.com>
2017-05-30 21:55:13 +0200

    Ajout de fonctions sendTextMessages dans webhook.js

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var PAGE_ACCESS_TOKEN = require('../config/default.json').pageAccessToken;
+var request = require('request');
 
 router.get('/', function(req, res) {
   console.log("fonction get");
